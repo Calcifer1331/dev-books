44e9375d8255d4461a0858f57af0d0a59a39a527
Yoseph Araúz <154104882+Calcifer1331@users.noreply.github.com>
2024-10-25 17:02:33 -0500

    se elimino las variables de entorno

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
 
-  integrations: [tailwind()],
-  //configurar bariable de entorno, en el .env
-  env: {
-    schema: {
-      SHOW_BUY_BUTTON: envField.boolean({ context: 'server', access: 'public' }),
-      SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public' })
-    }
-  }
+  integrations: [tailwind()]
 });
